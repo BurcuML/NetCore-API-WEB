28570e3d265d3f4f0edd542d86b40375e647c390
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-12-10 05:06:14 +0300

    updated

--- Repositories/Product.cs
+++ Repositories/Product.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace App.Repositories;
 
-namespace App.Repositories
+public class Product
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
 }
