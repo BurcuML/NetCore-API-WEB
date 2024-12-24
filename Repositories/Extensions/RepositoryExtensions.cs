e22be9f426acffb1175adce8663637e2ca94e034
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-12-25 01:11:53 +0300

    updated

--- Repositories/Extensions/RepositoryExtensions.cs
+++ Repositories/Extensions/RepositoryExtensions.cs
@@ -29,10 +29,13 @@ namespace App.Repositories.Extensions
             //Burada IProductRepository'i gördüğün zaman ProductRepository'den nesne örneği üreteceksin diyor
             //Yaşam döngüsü scoped request response a döndüğü anda ProductRepository bu nesnenin de response olması lazım
             //çünkü bu nesnelerde DbContext'ler var (DbContext'in kendisi Scoped)
+            //veritabanı varsa o business logic içerisinde mutlaka scoped olmalıdır
             //Repository'ler singloten olmaz ya da Transient olmaz fakat efCore için geçerli bu
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>)); //eğer IGenericRepository bu birden fazla generic alsaydı
                                                                                           //ortaya <,> bu şekilde bir virgül koyacaktık
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             return services; 
 
         }
